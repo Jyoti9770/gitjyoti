547213b315476e965ff068da1ce82d86d3038627
Jyoti Soni <ssoni672341@gmail.com>
2025-09-02 22:55:22 +0530

    msg

---
+++ ifelse.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int n;
+    printf("enter a number\n");
+    scanf("%d", &n);
+    if (n % 2 == 0){
+    printf("even\n");}
+    else {
+    printf("odd\n");}
+return 0;
+}
