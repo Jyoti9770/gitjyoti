14c83cd4f4e66fc0c55a0dac998e6424654897a8
Jyoti soni <ssoni672341@gmail.com>
2026-03-31 23:36:56 +0530

    c++ code

---
+++ for.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //print no from 1 to n
+    // for(int i=1;i<=10;i++){
+    // cout<<i<<" ";
+    // }
+
+    //sum of n numbers
+
+    // int n=5;
+    // int sum=0;
+    // for(int i=1;i<=n;i++){
+    //     sum = sum + i;
+    // }
+    //  cout << sum <<endl;
+
+    // print even numbers
+
+    // int n=10;
+    // for(int i=2; i<=10;i++){
+    //     if(i%2==0){
+    //         cout<<i<<" ";
+    //     }
+    // }
+
+    // print odd numbers
+
+    // int n=10;
+    // for(int i=1;i<=n;i++){
+    //     if(i%2!=0){
+    //         cout<<i<<" ";
+    //     }
+    // }
+
+    //print sum of n even nmbers
+
+    // int n=10;
+    // int sum=0;
+    // for(int i=2;i<=n;i++){
+    //     if(i%2==0){
+    //         sum=sum+i;
+    //         cout<<i<<" \n";
+    //     }
+    // }
+    // cout<<sum;
+   // print table of any number
+
+   int n ;
+   cout<<"enter a num:";
+   cin>>n;
+   for(int i=1;i<=10;i++){
+    cout<<n*i<<" \n";
+   }
+    return 0;
+}
